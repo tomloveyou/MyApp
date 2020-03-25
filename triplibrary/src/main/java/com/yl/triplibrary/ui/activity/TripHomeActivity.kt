910eff5c5ec112772004275e4ae1e359f6f49ece
杨磊 <847436762@qq.com>
2020-03-25 17:37:09 +0800

    1

---
+++ triplibrary/src/main/java/com/yl/triplibrary/ui/activity/TripHomeActivity.kt
@@ -0,0 +1,23 @@
+package com.yl.triplibrary.ui.activity
+
+import com.standards.library.base.BaseActivity
+import com.standards.library.base.BaseTitleBarActivity
+import com.yl.triplibrary.R
+
+class TripHomeActivity : BaseActivity() {
+    override fun getExtra() {
+
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_trip_home_main
+    }
+
+    override fun init() {
+
+    }
+
+    override fun setListener() {
+
+    }
+}
